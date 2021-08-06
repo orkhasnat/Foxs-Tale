dbb2f18e6741e704ad139a648710d5b4a1cc8730
orkhasnat <ubuntu@Orkhasnat.localdomain>
2021-08-06 17:58:32 +0600

    Version 0.21

--- make.ps1
+++ make.ps1
@@ -1,47 +1,51 @@
 ## MakeFile alternative
 
 function compilation{
-    echo "........compiling..........."
+    echo "........compilling.........."
     g++ -c .\src\*.cpp -I.\include
+    echo "..........moving............"
+    #Move-Item -Path *.o -Destination .\obj -force
     echo ".........linking............"    
-    g++ -o Play .\*.o -L.\lib -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system
+    #g++ -o RapidRoll .\obj\*.o main.o -L.\lib -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system
+    g++ -o RapidRoll .\*.o -L.\lib -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system
+    mv .\RapidRoll.exe .\bin -force
     echo "...........done............."
 }
 
 function cleaning{
-    echo "........cleaning............"
+    echo "........cleaning.........."
+    #rm .\obj\*.o
     rm .\*.o
     echo "...........done............."
 }
 
 function Helpmenu{
-        echo "Write -c for compilation"
-	echo "clean for deleting the .o files, -cl for .o and .exe file"
+        echo "Write -c for compilation, clean for deleting the .o files and cleanall for .o and .exe file"
         echo "And -e to execute the game"
-        echo "Or you can pass -a/all for all three (compile,clean,execute)"
+        echo "Or you can pass -a/all to all three (compile,clean,execute)"
 }
 
 function RunAgain{
-    $param2= Read-Host "Enter the command: "
+    $param2= Read-Host "Enter the command,sire!!"
     switch ($param2){
             "-h" {Helpmenu; Break}
             "help" {Helpmenu; Break}
             "compile" {compilation}
             "-c" {compilation}
             "clean" {cleaning}
-            "-cl" {cleaning ;rm Play.exe; Break}
-            "execute" {./Play.exe; Break}
-            "-e" {./Play.exe; Break}
+            "cleanall" {cleaning ;rm .\bin\RapidRoll.exe; Break}
+            "execute" {.\bin\RapidRoll.exe; Break}
+            "-e" {.\bin\RapidRoll.exe; Break}
             "all" {
                     compilation;
                     cleaning;
-                    ./Play.exe;
+                    .\bin\RapidRoll.exe;
                     Break
                     }
             "-a" {
                     compilation;
                     cleaning;
-                    ./Play.exe;
+                    .\bin\RapidRoll.exe;
                     Break
                 }
         }
@@ -56,19 +60,19 @@ switch ($params){
     "compile" {compilation}
     "-c" {compilation}
     "clean" {cleaning}
-    "-cl" {cleaning ;rm Play.exe; Break}
-    "execute" {./Play.exe; Break}
-    "-e" {./Play.exe; Break}
+    "cleanall" {cleaning ;rm .\bin\RapidRoll.exe; Break}
+    "execute" {.\bin\RapidRoll.exe; Break}
+    "-e" {.\bin\RapidRoll.exe; Break}
     "all" {
         compilation;
         cleaning;
-        ./Play.exe;
+        .\bin\RapidRoll.exe;
         Break
     }
     "-a" {
         compilation;
         cleaning;
-        ./Play.exe;
+        .\bin\RapidRoll.exe;
         Break
     }
 }
