f26a4b2c9a45a1ed02b88dc41fb9a2636c975900
orkhasnat <ubuntu@Orkhasnat.localdomain>
2021-08-08 04:54:44 +0600

    remove comment for no console

--- make.ps1
+++ make.ps1
@@ -7,7 +7,7 @@ function compilation{
     #Move-Item -Path *.o -Destination .\obj -force
     echo ".........linking............"    
     #g++ -o RapidRoll .\obj\*.o main.o -L.\lib -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system
-    g++ -o RapidRoll .\*.o -L.\lib -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system
+    g++ -o RapidRoll .\*.o -L.\lib -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system #-mwindows
     mv .\RapidRoll.exe .\bin -force
     echo "...........done............."
 }
